3b2a0479dee6198d6fd3ad0324d69391a0542997
tomatoTed <762496708@qq.com>
2016-12-23 18:00:26 +0800

    javaFx

---
+++ deployerFX/src/com/gx/javaFX/Main.java
@@ -0,0 +1,10 @@
+package com.gx.javaFX;
+
+/**
+ * Created by Administrator on 2016/12/23.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("javaFX");
+    }
+}
