5dca40dbbb77df2d38143524a7e7272d7a080d87
tomatoTed <762496708@qq.com>
2016-12-27 09:27:45 +0800

    删除

--- deployer/src/com/gx/javaDeployer/shutdown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tomcatDir=$1 #tomcat主目录
-
-cd $tomcatDir
-echo "shutdown server"
-sh bin/shutdown.sh
-echo 'finished shutdown server'
-echo 'exit_qwertyuiop'
--- deployer/start.cmd
+++
@@ -1,1 +0,0 @@
-java -jar deployer.jar config.properties
